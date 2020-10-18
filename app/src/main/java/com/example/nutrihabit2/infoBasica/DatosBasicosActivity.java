da8396646d36f5651913c0143be265ff73f34b3a
Fernando <luisfgomezg@unicauca.edu.co>
2020-10-18 12:37:22 -0500

    Creacion Activity Datos Basicos

---
+++ app/src/main/java/com/example/nutrihabit2/infoBasica/DatosBasicosActivity.java
@@ -0,0 +1,14 @@
+package com.example.nutrihabit2.infoBasica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatosBasicosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_basicos);
+    }
+}
