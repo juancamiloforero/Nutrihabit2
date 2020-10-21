4dd94289ada14f27219ee671cd9f1c0c5d34cab2
Fher95 <luisfgomezg@unicauca.edu.co>
2020-10-20 19:58:06 -0500

    Conexión con la base de datos establecida

--- app/src/main/java/com/example/nutrihabit2/bienvenidaActivity.java
+++ app/src/main/java/com/example/nutrihabit2/bienvenidaActivity.java
@@ -2,9 +2,14 @@ package com.example.nutrihabit2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import com.example.nutrihabit2.alimentos.AlimentosCrearActivity;
+import com.example.nutrihabit2.infoBasica.DatosBasicosActivity;
 
 public class bienvenidaActivity extends AppCompatActivity {
 
@@ -18,13 +23,22 @@ public class bienvenidaActivity extends AppCompatActivity {
         switch(view.getId()) {
             case (R.id.btnGanarMasa) :
                 Toast.makeText(this,"MASA MUSCULAR",Toast.LENGTH_SHORT).show();
+                this.irADatosBasicos(1);
                 break;
             case (R.id.btnPerderPeso) :
                 Toast.makeText(this,"PERDER PESO",Toast.LENGTH_SHORT).show();
+                this.irADatosBasicos(2);
                 break;
             case (R.id.btnMantenerEstado) :
                 Toast.makeText(this,"MANTENER ESTADO",Toast.LENGTH_SHORT).show();
+                this.irADatosBasicos(3);
                 break;
         }
+    }
+
+    public void irADatosBasicos(int parSeleccion) {
+        Log.d("TAG3", "NextClicked");
+        Intent intent = new Intent(this, DatosBasicosActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/nutrihabit2/infoBasica/DatosBasicosActivity.java
+++ app/src/main/java/com/example/nutrihabit2/infoBasica/DatosBasicosActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.nutrihabit2.R;
+
 public class DatosBasicosActivity extends AppCompatActivity {
 
     @Override
