b14b8320416d68c5638d1af570801f197127e293
DESKTOP-F1FP9J8\camil <juancamiforero@unicauca.edu.co>
2020-10-21 19:42:56 -0500

    Redirección botón de guardar consumo a seguimiento

--- app/src/main/java/com/example/nutrihabit2/consumoDiario/ConsumoRegistroActivity.java
+++ app/src/main/java/com/example/nutrihabit2/consumoDiario/ConsumoRegistroActivity.java
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import com.example.nutrihabit2.R;
 import com.example.nutrihabit2.alimentos.Alimento;
 import com.example.nutrihabit2.alimentos.FragmentListaAlimentos;
+import com.example.nutrihabit2.seguimiento.SeguimientoListaActivity;
 
 import java.util.ArrayList;
 
-public class ConsumoRegistroActivity extends AppCompatActivity {
+public class ConsumoRegistroActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mButtonGuardarConsumo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +31,15 @@ public class ConsumoRegistroActivity extends AppCompatActivity {
         FragmentListaAlimentos frag = FragmentListaAlimentos.newInstance(1);
         ft.replace(R.id.listaAlimentosContainer, frag);
         ft.commitNow();
+
+        mButtonGuardarConsumo = findViewById(R.id.btGuardarConsumo);
+
+        mButtonGuardarConsumo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, SeguimientoListaActivity.class);
+        startActivity(intent);
     }
 }
