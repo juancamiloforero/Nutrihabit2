bf46cccc238483b68d1793e4604aaf24416e46b0
DESKTOP-F1FP9J8\camil <juancamiforero@unicauca.edu.co>
2020-10-24 09:11:07 -0500

    Crear usuario base creado

--- app/src/main/java/com/example/nutrihabit2/MainActivity.java
+++ app/src/main/java/com/example/nutrihabit2/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.nutrihabit2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -25,12 +27,17 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String mPrefs = "USER_INFORMATION";
+    private String keyUserId = "userId";
+
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.verifyUser();
 
         // this.ejemploEscrituraBD();
         /*
@@ -47,6 +54,53 @@ public class MainActivity extends AppCompatActivity {
         Intent intent3 = new Intent(this, SeguimientoListaActivity.class);
         startActivity(intent3);*/
 
+    }
+
+    private void verifyUser() {
+        SharedPreferences sharedPref = getSharedPreferences(this.mPrefs, Context.MODE_PRIVATE);
+        String defaultID = getResources().getString(R.string.defaultUserId);
+        String userID = sharedPref.getString(this.keyUserId, defaultID);
+
+        if (!userID.equals(defaultID)) {
+            Log.d("TAGSharedPreferences", "UserID: " + userID);
+        } else {
+            crearUsuarioFirebase();
+
+            // ToDo: crearUsuarioFirebase(User object);
+        }
+    }
+
+    private void saveLocalUser(String id) {
+        SharedPreferences sharedPref = getSharedPreferences(this.mPrefs, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(this.keyUserId, id);
+        editor.apply();
+        Log.d("TAG", "Se creó el usuario con el ID" + id);
+    }
+
+    private void crearUsuarioFirebase() {
+        Map<String, Object> user = new HashMap<>();
+        user.put("estatura", 1.84);
+        user.put("peso", 68);
+        user.put("genero", true);
+        user.put("edad", 21);
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+
+                        saveLocalUser(documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("TAG", "Error adding document", e);
+                    }
+                });
     }
 
     private void ejemploEscrituraBD() {
