1bd2f99c830637aadd1565cf475754714e011b03
Fher95 <luisfgomezg@unicauca.edu.co>
2020-10-31 18:03:22 -0500

    Avance en fragment perfil

--- app/src/main/java/com/example/nutrihabit2/menuPrincipal/ui/perfil/PerfilFragment.java
+++ app/src/main/java/com/example/nutrihabit2/menuPrincipal/ui/perfil/PerfilFragment.java
@@ -22,13 +22,7 @@ public class PerfilFragment extends Fragment {
         perfilViewModel =
                 new ViewModelProvider(this).get(PerfilViewModel.class);
         View root = inflater.inflate(R.layout.fragment_perfil, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        perfilViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
