6ca3d6be129af6672a496c4011ee74111d438f68
estivenp <estivenp@unicauca.edu.co>
2020-11-01 19:37:04 -0500

    ajustes en el logo de la aplicacion

--- app/src/main/java/com/example/nutrihabit2/menuPrincipal/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/nutrihabit2/menuPrincipal/ui/login/LoginFragment.java
@@ -62,7 +62,7 @@ public class LoginFragment extends Fragment {
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
         SignInButton signInButton = (SignInButton)root.findViewById(R.id.sign_in_button);
-        this.setGoogleButtonText(signInButton,"Iniciar sesión con google");
+        this.setGoogleButtonText(signInButton,R.string.f_login_iniciar_sesion);
        // GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getActivity());
         signInButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -144,7 +144,7 @@ public class LoginFragment extends Fragment {
         }
     }*/
 
-    protected void setGoogleButtonText(SignInButton signInButton, String buttonText) {
+    protected void setGoogleButtonText(SignInButton signInButton, int buttonText) {
         // Encontrar el texto que esta dentro del boton
         for (int i = 0; i < signInButton.getChildCount(); i++) {
             View v = signInButton.getChildAt(i);
